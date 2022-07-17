86b5763e4c9b2e81b4e1a9dd849f8f3a0e56cbf2
06000208 <52764066+06000208@users.noreply.github.com>
2022-07-17 11:34:36 -0500

    scripts

---
+++ modules/scripts/start.js
@@ -0,0 +1,18 @@
+/**
+ * Startup message with version info, this is in it's own script to ensure it is
+ * logged first
+ * @module scripts/start
+ */
+
+import process from "node:process";
+import { version as discordVersion } from "discord.js";
+import { name, version as packageVersion } from "../constants.js";
+import { log } from "../log.js";
+
+log.info({
+    "nodeVersion": process.versions.node,
+    "discordVersion": discordVersion,
+    "version": packageVersion,
+    "platform": process.platform,
+    "dev": process.env.dev?.toLowerCase() === "true",
+}, `Starting ${name}`);
