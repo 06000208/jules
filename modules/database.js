443c11de88a9136fb73b5cd148648a90f5d026cb
06000208 <52764066+06000208@users.noreply.github.com>
2022-07-17 11:34:55 -0500

    Create database.js

---
+++ modules/database.js
@@ -0,0 +1,13 @@
+import { join } from "node:path";
+import { Low, JSONFile } from "lowdb";
+import { log } from "./log.js";
+import { name, directory, defaultData } from "./constants.js";
+
+const database = new Low(new JSONFile(join(directory, "data", `${name}.json`)));
+await database.read();
+if (!database.data) database.data = { ...defaultData };
+await database.write();
+
+log.info("Started database");
+
+export { database };
