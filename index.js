844bb5026ea2ff14afe052e27d66c5b607d64aae
06000208 <52764066+06000208@users.noreply.github.com>
2022-07-17 15:29:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // order of import statements matters, loaded asynchronously
-
 import "./modules/scripts/processEvents.js"; // Node.js process events script
 import "./modules/scripts/start.js"; // Starting message
 import "./modules/scripts/env.js"; // Populates environment variables script
-import "./modules/discord.js"; // Start bot
 import "./modules/database.js"; // Start database
+import "./modules/discord.js"; // Start bot
+import "./modules/scripts/deployCommands.js"; // Deploy commands
