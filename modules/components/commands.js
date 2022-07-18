61cde5722b10cb7b105994ee6d2e33bcade0f9ed
06000208 <52764066+06000208@users.noreply.github.com>
2022-07-17 22:00:02 -0500

    Update commands.js

--- modules/components/commands.js
+++ modules/components/commands.js
@@ -3,6 +3,7 @@ import { Permissions } from "discord.js";
 
 export const commands = [
     new SlashCommandBuilder().setName("about").setDescription("basic info about the bot"),
+    new SlashCommandBuilder().setName("help").setDescription("alias for /about"),
     new SlashCommandBuilder().setName("ping").setDescription("checks latency"),
     new SlashCommandBuilder().setName("exit").setDescription("exits process peacefully"),
 ].map(command => command.toJSON());
@@ -11,6 +12,6 @@ export const guildCommands = [
     new SlashCommandBuilder().setName("clear")
         .setDescription("clears a channel of messages from a user")
         .setDefaultMemberPermissions(Permissions.FLAGS.MANAGE_MESSAGES)
-        .addChannelOption(option => option.setName("channel").setDescription("Select a channel").setRequired(true))
-        .addUserOption(option => option.setName("target").setDescription("Select a user").setRequired(true)),
+        .addChannelOption(option => option.setName("channel").setDescription("select a channel").setRequired(true))
+        .addUserOption(option => option.setName("user").setDescription("select a user").setRequired(true)),
 ].map(command => command.toJSON());
