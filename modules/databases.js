a84803f6406815a967a447945280733d383bad8c
06000208 <52764066+06000208@users.noreply.github.com>
2022-07-21 12:54:31 -0500

    split databases

---
+++ modules/databases.js
@@ -0,0 +1,29 @@
+import { join } from "node:path";
+import { Low, JSONFile } from "lowdb";
+import { log } from "./log.js";
+import { directory } from "./constants.js";
+
+/**
+ * lowdb json database used for analytics collected during recursively scanning
+ * channels
+ */
+const analytics = new Low(new JSONFile(join(directory, "data", "data.json")));
+await analytics.read();
+if (!analytics.data) {
+    analytics.data = {};
+    await analytics.write();
+}
+
+/**
+ * lowdb json database used for saving emoji data
+ */
+const emojis = new Low(new JSONFile(join(directory, "data", "emojis.json")));
+await emojis.read();
+if (!emojis.data) {
+    analytics.data = {};
+    await emojis.write();
+}
+
+log.info("Started databases");
+
+export { analytics, emojis };
