7fd3622e92ff86facab47e09d02520d992bdc788
06000208 <52764066+06000208@users.noreply.github.com>
2022-07-21 12:54:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 import "./modules/scripts/processEvents.js"; // Node.js process events script
 import "./modules/scripts/start.js"; // Starting message
 import "./modules/scripts/env.js"; // Populates environment variables script
-import "./modules/database.js"; // Start database
+import "./modules/databases.js"; // Start database
 import "./modules/discord.js"; // Start bot
